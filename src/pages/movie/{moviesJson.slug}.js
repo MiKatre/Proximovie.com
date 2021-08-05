487b99e68f84b19e07c1be6f6e882e43a66b4c07
Micael de Prado <0mica4420@gmail.com>
2021-08-05 19:09:43 +0200

    Add first iteration of related movies

--- src/pages/movie/{moviesJson.slug}.js
+++ src/pages/movie/{moviesJson.slug}.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { graphql } from 'gatsby' 
+import { graphql, Link } from 'gatsby' 
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { Container, Row } from 'react-bootstrap'
 import { Button } from 'react-bootstrap'
@@ -9,17 +9,27 @@ const BlogPost = ({data}) => {
   console.log(data)
   const movie = data.allMoviesJson.edges[0].node
   const image = getImage(movie.gatsby_image_path)
+  const relatedByCast = movie.related_by_cast.map(i => (
+    <li>
+      <Link to={`/movie/${i.slug}`}>{i.title}</Link>
+    </li>
+  ))
   return (
     <Layout>
       <div className="">
         <div className="m-auto text-center bg-dark py-2">
-          <GatsbyImage className="shadow" image={image} />
+          <GatsbyImage className="shadow" image={image} alt=""/>
         </div>
 
         <Container fluid className="">
           <Row>
             <h1 className="fw-bolder text-center pt-5">Similar movies</h1>
             <p className="text-center">Best movies like <em>{movie.title}</em></p>
+            <div>
+              <ul>
+                {relatedByCast}
+              </ul>
+            </div>
           </Row>
           <Row>
             <h1 className="fw-bolder text-center pt-5">Movies with similar <em className="text-secondary">posters</em> </h1>
@@ -46,6 +56,18 @@ export const query = graphql`
         node {
           title
           release_date(formatString: "MMMM D, YYYY")
+          related_by_cast {
+            title
+            slug
+            gatsby_image_path {
+              childImageSharp {
+                gatsbyImageData(
+                  width: 200
+                  placeholder: BLURRED
+                )
+              }
+            }
+          }
           gatsby_image_path {
             childImageSharp {
               gatsbyImageData(
