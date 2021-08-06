3f9ecbdf0ab99dd5f7d3ad74253c15c888a105df
Micael de Prado <0mica4420@gmail.com>
2021-08-06 12:33:28 +0200

    Good looking homepage with jumbotron

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
 import MovieList from '../components/MovieList'
 import { Container, Row } from 'react-bootstrap'
+import Jumbotron from '../components/Jumbotron'
 
 const IndexPage = ({data}) => {
   console.log(data)
@@ -11,22 +12,19 @@ const IndexPage = ({data}) => {
 
   return (
     <Layout>
-      <div>
-        <Container  className="">
-          <Row>
-            <h1 className="fw-bolder text-center pt-4 pb-3">Recent Movies</h1>
-            {/* <p className="text-center">Best movies like </p> */}
-            <MovieList movies={recent}/>
-          </Row>
-          <Row>
-            <h1 className="fw-bolder text-center pt-5 pb-3">Most popular movies</h1>
-            {/* <p className="text-center">Best movies like </p> */}
-            <MovieList movies={budget}/>
-          </Row>
-        </Container>
-      </div>
-
-
+      <Jumbotron/>
+      <Container  className="">
+      <Row>
+          <h1 id="popular_movies" className="fw-bolder text-center pt-5 pb-3">Most popular movies</h1>
+          {/* <p className="text-center">Best movies like </p> */}
+          <MovieList movies={budget}/>
+        </Row>
+        <Row>
+          <h1 id="recent_movies"  className="fw-bolder text-center pt-4 pb-3">Recent Movies</h1>
+          {/* <p className="text-center">Best movies like </p> */}
+          <MovieList movies={recent}/>
+        </Row>
+      </Container>
     </Layout>
   )
 }
