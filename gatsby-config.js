3473f39b5d435276383d4d7d9325caec2be91555
Micael de Prado <0mica4420@gmail.com>
2021-08-22 23:40:52 +0200

    Replace GA4 by older but better supported GA3

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-0V72YY2LV1", // Google Analytics / GA
+          "UA-78677033-8", // Google Analytics / GA
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
