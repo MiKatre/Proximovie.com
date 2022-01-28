23adbe5dac77876d0e6a8391d1e1d29f98352038
Micael de Prado <0mica4420@gmail.com>
2022-01-28 11:46:42 +0100

    Reset package versions as they were before trying v4

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,17 +13,17 @@ module.exports = {
       resolve: `gatsby-plugin-sharp`,
       options: {
         defaults: {
-          formats: [`auto`, `webp`],
+          // formats: [`auto`, `webp`],
           placeholder: `dominantColor`,
-          quality: 50,
-          breakpoints: [750, 1080, 1366, 1920],
-          backgroundColor: `transparent`,
-          tracedSVGOptions: {},
-          blurredOptions: {},
-          jpgOptions: {},
-          pngOptions: {},
-          webpOptions: {},
-          avifOptions: {},
+          // quality: 50,
+          // breakpoints: [750, 1080, 1366, 1920],
+          // backgroundColor: `transparent`,
+          // tracedSVGOptions: {},
+          // blurredOptions: {},
+          // jpgOptions: {},
+          // pngOptions: {},
+          // webpOptions: {},
+          // avifOptions: {},
         }
       }
     },
